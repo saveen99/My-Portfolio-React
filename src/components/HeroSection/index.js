6d1696a669dce175f67d67156202fa357899084e
saveen-maduranga <saveenonline@gmail.com>
2024-05-19 17:54:29 +0530

    Updated Files

--- src/components/HeroSection/index.js
+++ src/components/HeroSection/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import HeroBgAnimation from '../HeroBgAnimation'
 import { HeroContainer, HeroBg, HeroLeftContainer, Img, HeroRightContainer, HeroInnerContainer, TextLoop, Title, Span, SubTitle,SocialMediaIcons,SocialMediaIcon, ResumeButton } from './HeroStyle'
-import HeroImg from '../../images/HeroImage.jpg'
+import HeroImg from '../../images/saveen.jpg'
 import Typewriter from 'typewriter-effect';
 import { Bio } from '../../data/constants';
 
--- src/data/constants.js
+++ src/data/constants.js
@@ -1,20 +1,19 @@
 export const Bio = {
-  name: "Rishav Chanda",
+  name: "Saveen Maduranga",
   roles: [
-    "Full Stack Developer",
-    "Android Developer",
-    "UI/UX Designer",
+    "Front-End Developer",
+    "Web Developer",
     "Programmer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
-  github: "https://github.com/rishavchanda",
+  github: "https://github.com/saveen-maduranga",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
-  linkedin: "https://www.linkedin.com/in/rishav-chanda-b89a791b3/",
-  twitter: "https://twitter.com/RishavChanda",
-  insta: "https://www.instagram.com/rishav_chanda/",
-  facebook: "https://www.facebook.com/rishav.chanda.165/",
+    "https://drive.google.com/file/d/1_S-65fTN_9Td3LRhMAWpJ8PRgxcr91Hx/view?usp=sharing",
+  linkedin: "https://www.linkedin.com/in/saveen-maduranga-939250181/",
+  twitter: "https://twitter.com/saveen.maduranga",
+  insta: "https://www.instagram.com/saveen.maduranga/",
+  facebook: "https://www.facebook.com/saveen.maduranga/",
 };
 
 export const skills = [
