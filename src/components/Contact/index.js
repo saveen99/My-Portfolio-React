2fbaf0cb64c0005e8d7a16ac8238a22b92e496b4
saveen-maduranga <saveenonline@gmail.com>
2024-05-27 15:06:59 +0530

    Update index.js

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -130,7 +130,7 @@ const Contact = () => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    emailjs.sendForm('service_r9wvzpc', 'template_1d48cff', form.current, 'RPAKJtm3mBnhm4wTD5dL5')
+    emailjs.sendForm('service_r9wvzpc', 'template_1d48cff', form.current, 'qqfB0_zchgSPidvhs')
       .then((result) => {
         setOpen(true);
         form.current.reset();
