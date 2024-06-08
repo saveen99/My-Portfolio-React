5bdd31fef5f59f1af0fc1d34a611bb266dd44ed6
saveen-maduranga <saveenonline@gmail.com>
2024-06-08 12:08:14 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/saveen-maduranga",
   resume:
-    "https://drive.google.com/file/d/1RbJ8tHC3Jbma0N2PBmvaYyB_P9_QBKHI/view?usp=sharing",
+    "https://drive.google.com/file/d/1iIXqcY0AzjbZgyykjZkiYXlNovhabFVF/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/saveen-maduranga-939250181/",
   twitter: "https://twitter.com/saveen.maduranga",
   insta: "https://www.instagram.com/saveen.maduranga/",
