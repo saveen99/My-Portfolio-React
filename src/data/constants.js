06563d6de711e09bb7a6dce15732e33242f599da
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-20 10:29:27 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -1,9 +1,9 @@
 export const Bio = {
   name: "Saveen Maduranga",
   roles: [
-    "Front-End Developer",
+    "Software Developer",
     "Web Developer",
-    "Programmer",
+    "Mobile App Developer",
   ],
   description:
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
