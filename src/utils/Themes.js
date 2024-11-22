6ab187b90880c8bfc2401bce293f5b78f64104ed
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2024-11-22 09:38:38 +0530

    Update Colors

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -1,12 +1,12 @@
 export const darkTheme = {
     bg:"#1C1C27",
     bgLight: "#1C1E27",
-    primary:"#854CE6",
+    primary:"#009E60",
     text_primary:"#F2F3F4",
     text_secondary:"#b1b2b3",
     card:"#171721",
     card_light: '#191924',
-    button:"#854CE6",
+    button:"#009E60",
     white:"#FFFFFF",
     black:"#000000",
 }
@@ -14,9 +14,9 @@ export const darkTheme = {
 export const lightTheme = {
     bg:"#FFFFFF",
     bgLight: "#f0f0f0",
-    primary:"#be1adb",
+    primary:"#009E60",
     text_primary:"#111111",
     text_secondary:"#48494a",
     card:"#FFFFFF",
-    button:"#5c5b5b",
+    button:"#009E60",
 }
