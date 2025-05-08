7af8413d5f7d09bf1ec5f54e5fd6a1f6f9c09537
Saveen Maduranga <105966451+saveen99@users.noreply.github.com>
2025-05-08 11:16:42 +0530

    Update constants.js

--- src/data/constants.js
+++ src/data/constants.js
@@ -9,7 +9,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/saveen99",
   resume:
-    "https://drive.google.com/file/d/1irpN8S_9n9wcskXW4wmAsKgF2_e91KTN/view?usp=sharing",
+    "https://drive.google.com/file/d/14hyCYt5LYRUJNPu6DPkGZaQJHVWdRnVy/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/saveen-maduranga-939250181/",
   twitter: "https://twitter.com/saveen.maduranga",
   insta: "https://www.instagram.com/saveen.maduranga/",
